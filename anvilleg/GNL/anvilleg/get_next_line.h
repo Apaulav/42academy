3146abbd4c0e53f826fcab75f6cf9d00e962beaa
Apaulav <50292334+Apaulav@users.noreply.github.com>
2020-01-30 19:35:41 +0100

    update

---
+++ anvilleg/GNL/anvilleg/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anvilleg <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/30 17:34:17 by anvilleg          #+#    #+#             */
+/*   Updated: 2020/01/30 18:14:20 by anvilleg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 32
+#endif
+
+int get_next_line(int fd, char **line);
+
