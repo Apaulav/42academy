64bf92c94271a55eb1a32209d2394f85be203c26
Apaulav <paulaville@gmail.com>
2020-01-30 08:18:52 +0100

    creando el bucle

--- anvilleg/GNL/anvilleg/get_next_line.c
+++ anvilleg/GNL/anvilleg/get_next_line.c
@@ -6,34 +6,80 @@
 /*   By: anvilleg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 11:25:26 by anvilleg          #+#    #+#             */
-/*   Updated: 2020/01/26 19:47:25 by pau              ###   ########.fr       */
+/*   Updated: 2020/01/29 21:54:34 by pau              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#define BUFFER_SIZE 32
 
 int get_next_line(int fd, char **line)
 {
 	ssize_t bytes;
-	char buf[50];
+	static char *buf;
 
-	line = 0;
+	line = NULL;
+	buf = NULL;
+	bytes = 0;
+	buf = (char *)malloc(sizeof(char)*(BUFFER_SIZE+1));
+	if (buf == NULL)
+		return (-1);
 	if (fd == -1)
 	{
 		printf("Error al abrir archivo.\n");
-		return (fd);
+		return (-1);
 	}
 	else
 	{
-		bytes = read(fd, buf, 50);
-		close(fd);
+		bytes = read(fd, buf, BUFFER_SIZE);
+		buf[bytes] = '\0';
 		if (bytes == 1 )
+		{
 			printf("Archivo vacio \n");
+			return (-1);
+		}
 		else 
-			printf("Caracteres: %zd \nArchivo: \n%s \n", bytes, buf);
+		{
+/*			line = (char **)malloc(sizeof(char)*BUFFER_SIZE);
+			if (line == NULL)
+				return (-1);
+*/
+			//mientras archivo no termine line [i][j]
+			//mientras salto de linea no exista
+			//devuelve line [i][j] y a j++
+			// i++
+			int i = 0;
+			int j = 0;
+			int h = 0;
+			while (buf[h] != '\0' /*h < 23*/)
+			{
+				while (buf[h] != '\n')
+				{
+				//line[0][0] = buf[0];
+					printf("Buf: %c\n", buf[h]);
+//					write(1,buf+h,1);
+//					write(1,"\n",1);
+				//printf("%s",line[0][0]);
+					j++;
+					h++;
+				}
+				h++;
+				i++;
+				j=0;
+			}
+//			int i = 0;
+//			printf("i:%d", i);
+			printf("Caracteres: %zd \nArchivo: \n%s", bytes, buf);
+//			i++;
+//			printf("i:%d", i);
+		}
 	}
+	close(fd);
+	free(buf);
+	free(line);
 	return 0;
 }
 
@@ -44,7 +90,7 @@ int main(int argc, char **argv)
 //	if(get_next_line(1,&str) != -1)
 //		printf("%s", str);
 	int fd;
-	char **line;
+	char **line = NULL;
 	
 	//abro archivo como solo lectura (O_RDONLY) y me devuelve el fd
 	fd = open(argv[1], O_RDONLY);
