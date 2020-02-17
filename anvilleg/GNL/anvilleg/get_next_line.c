a218cfe5a3c0b7abe7688220ff8fdcb521ef59f1
Apaulav <paulaville@gmail.com>
2020-02-17 01:36:15 +0100

    line

--- anvilleg/GNL/anvilleg/get_next_line.c
+++ anvilleg/GNL/anvilleg/get_next_line.c
@@ -6,23 +6,53 @@
 /*   By: anvilleg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 11:25:26 by anvilleg          #+#    #+#             */
-/*   Updated: 2020/02/13 12:06:55 by pau              ###   ########.fr       */
+/*   Updated: 2020/02/17 01:31:04 by pau              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int rfile(int fd, char *buf,char *copybuf, char **line)
+char resttemp(char *copybuf, int i, char *rest)
+{
+	int len;
+	int j;
+
+	len = i;
+	j = 0;
+	printf ("restcopy: %s\n", copybuf);
+	rest = (char *)malloc(sizeof(char) * i);
+	printf("lenrest: %d",len);
+	printf("copybufrest: %s\n",copybuf[len]);
+	while (copybuf[len] != 'e' && copybuf[len] != '\0')
+	{
+		printf ("restcopy: %s\n", copybuf);
+		rest[j] = copybuf[len];
+		printf("rest: %s\n", rest);
+		j++;
+		len--;
+	}
+}
+
+int rfile(int fd, char *buf, char **line)
 {
 	ssize_t bytes;
+	static char *copybuf;
 	char *temp;
+	char *rest;
 	int m;
+    int i;
+	int l;
+	int t;
 
 	if (!copybuf)
 		copybuf = ft_strdup("");
+//	printf("longitud copybuf: %d\n", ft_strlen(copybuf));
+	temp = ft_strdup("");
 	while (buf)
 	{
-		temp = copybuf;
+		i = ft_strlen(copybuf);
+		resttemp(copybuf,i,rest);
+		printf("longitud2 copybuf: %d\n", ft_strlen(copybuf));
 		bytes = read(fd, buf, BUFFER_SIZE);
 		if (bytes <= 0 )
     	{
@@ -33,29 +63,56 @@ int rfile(int fd, char *buf,char *copybuf, char **line)
 		else
 	    {
     		buf[bytes] = '\0';
-			copybuf = ft_strjoin(temp, buf);
-        	printf("Caracteres: %zd \nArchivo: \n%s", bytes, buf);
-			printf("\ncopy: %s\n",copybuf);
+			printf("Leido: %s\n",buf);
+			copybuf = ft_strjoin(copybuf, buf);
+//        	printf("Caracteres leidos: %zd\nleido: %s\n", bytes, buf);
+			printf("copia: %s\n",copybuf);
 			//printf("salto: %s\n",ft_strchr(copybuf,'\n'))    ;
-            int i = 0;
-            while (copybuf[i] != '\n' && copybuf[i])
+			//otra función comprobar \n
+            t = ft_strlen(temp);
+			printf("t: %d\n",t);
+			while (copybuf[i] != '\n' && copybuf[i])
 			{
-	             //printf("i: %c\n",copybuf[i]);
-                 i++;
+				temp[t] = copybuf[i];
+				//printf("i: %c\n",copybuf[i]);
+                i++;
+				t++;
             }
+			t++;
+			temp[t] = '\0';
+			printf("temp despues: %s\n",temp);
             if (copybuf[i] == '\n')
             {
-	            m = i+1;
-				line =(char **)malloc(sizeof(char)*(ft_strlen(temp)));
-                temp = ft_strdup(&copybuf[m]);
-                printf("temp: %s\n",temp);
-				//line = ft_strdup(copybuf);
+
+	        	m = i+1;
+				printf("Salto de línea encontrado en la posicion: %d\n", m);
+				//line = (char **)malloc(sizeof(char *)*m);
+				//if (line == NULL)
+				//	return (-1);
+				//l = 0;
+				//t = 0;
+				//while (temp[t] != '\0')
+				//{
+				//	line[0][l] = temp[t];
+				//	t++;
+				//	l++;
+				//}
+
+				//temp = ft_strdup(&copybuf[m]);
+				//printf("temporal:%s\n", temp);
+				//line = ft_substr(copybuf,0,m);	
+				line = ft_strdup(temp);
 				//free(temp);
 				printf("Line: %s\n",line);
+				//copybuf[i]='\0';
+				printf("copia sin \\n: %s\n",temp);
+				//break;
+
+				return (1);
             }
-			//return(1);
 		}
 	}
+				//return (1);
 }
 
 int get_next_line(int fd, char **line)
@@ -63,7 +120,6 @@ int get_next_line(int fd, char **line)
 	//recived fd and create buffer(used for keep the text reading.)
 	char *buf;
 	//declarar varible estática
-	static char *copybuf;
 	
 	line = NULL;
 	if (fd == -1)
@@ -82,9 +138,10 @@ int get_next_line(int fd, char **line)
 		buf = (char *)malloc(sizeof(char)*(BUFFER_SIZE+1));
 		if (buf == NULL)
 			return (-1);
-		rfile(fd, buf,copybuf,line);
+		rfile(fd, buf,line);
 		//Llamo a función asignar **line
 	}
+	return(0);
 }
 
 int main(int argc, char **argv)
@@ -99,9 +156,13 @@ int main(int argc, char **argv)
 	//abro archivo como solo lectura (O_RDONLY) y me devuelve el fd
 	fd = open(argv[1], O_RDONLY);
 	printf("FD: %d \n",fd);
+	printf("Primer gnl\n");
 	get_next_line(fd, &line);
+	printf("Segundo gnl\n");
 	get_next_line(fd, &line);
+	printf("Tercer gnl\n");
 	get_next_line(fd, &line);
-	close(fd);
-
+	printf("cuarto gnl\n");
+	get_next_line(fd, &line);
+	//close(fd);
 }
--- anvilleg/GNL/anvilleg/get_next_line_utils.c
+++ anvilleg/GNL/anvilleg/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: anvilleg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/11 08:05:38 by anvilleg          #+#    #+#             */
-/*   Updated: 2020/02/11 08:06:42 by anvilleg         ###   ########.fr       */
+/*   Updated: 2020/02/16 11:33:43 by pau              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,4 +104,29 @@ char    *ft_strchr(const char *s, int c)
 	if (s[i] == c)
 		return ((char *)s + i);
 	return (NULL);
+}
+
+char    *ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char    *m;
+	size_t  i;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	m = (char *)malloc(sizeof(char) * (len + 1));
+	if (m == 0)
+		return (NULL);
+	if (ft_strlen(s) == 0)
+		return (ft_strdup(""));
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	while (i < len)
+	{
+		m[i] = s[start];
+		start++;
+		i++;
+	}
+	m[i] = '\0';
+	return (m);
 }
