58f0bbfa9dcf87efd587202f09f65a3336e8e70b
Apaulav <50292334+Apaulav@users.noreply.github.com>
2020-02-25 20:08:54 +0100

    funciona, falta normi

--- anvilleg/GNL/anvilleg/get_next_line.c
+++ anvilleg/GNL/anvilleg/get_next_line.c
@@ -6,64 +6,60 @@
 /*   By: anvilleg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 11:25:26 by anvilleg          #+#    #+#             */
-/*   Updated: 2020/02/25 00:11:13 by anvilleg         ###   ########.fr       */
+/*   Updated: 2020/02/25 20:06:13 by anvilleg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int rfile(int fd, char *buf, char **line)
+static	int		rfile(int fd, char *buf, char **line)
 {
-	ssize_t bytes;
-	static char *copybuf;
-	char *temp;
-    int i;
+	ssize_t		bytes;
+	static char	*copybuf;
+	char		*temp;
+	int			i;
+	char		*aux;
 
 	if (!copybuf)
-		copybuf = ft_strdup(""); //Compruebo si copybuf está vacio y lo inicializo
-	temp = ft_strdup("");
+		copybuf = NULL;
+	temp = NULL;
 	while (buf)
 	{
 		i = 0;
-		//printf("longitud2 copybuf: %zu\n", ft_strlen(copybuf));
 		bytes = read(fd, buf, BUFFER_SIZE);
-		printf("%ld", bytes);
-		if (bytes < 0/* && ft_strlen(copybuf) <= 0*/)
+		if (bytes < 0)
+		{
+			free(copybuf);
+			copybuf = NULL;
+			free(buf);
+			buf = NULL;
 			return (-1);
+		}
 		else if (bytes > 0 || ft_strlen(copybuf) > 0)
-        {
-            buf[bytes] = '\0';
-            //printf("Leido: %ld \"%s\"\n",bytes,buf);
-            //printf("co+le: \"%s-%s\"\n", copybuf,buf);
-            copybuf = ft_strjoin(copybuf, buf);
-            //printf("copia: \"%s\"\n\n",copybuf);
-            while (copybuf[i] != '\n' && copybuf[i])
-                i++;
-            //printf("temp despues: %s\n",temp);
-            if (copybuf[i] == '\n' || (copybuf[i] == 0 && bytes == 0))
-            {
-                //printf("%c", copybuf[i]);
-				free(temp);
+		{
+			buf[bytes] = '\0';
+			aux = ft_strjoin(copybuf, buf);
+			free(buf);
+			buf = NULL;
+			free(copybuf);
+			copybuf = NULL;
+			copybuf = ft_strdup(aux);
+			free(aux);
+			aux = NULL;
+			buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+			while (copybuf[i] != '\n' && copybuf[i])
+				i++;
+			if (copybuf[i] == '\n' || (copybuf[i] == 0 && bytes == 0))
+			{
+				if (temp)
+					free(temp);
 				if (copybuf[i] == '\n')
-				{
-					printf("pene");
 					temp = ft_strdup(&copybuf[i + 1]);
-				}
 				else
-				{
-					printf("vagina");
-					temp  = ft_strdup("");
-				}
-				if (!temp)
-				{
-					return (-1);
-				}
-                //copybuf[i] = '\0';
-               //printf("temporal: %s\n", temp);
-                *line = ft_substr(copybuf,0,i);
+					temp = ft_strdup("");
+				*line = ft_substr(copybuf, 0, i);
 				if (!(*line))
 					return (-1);
-                //printf("Line: %s\n",*line);
 				free(copybuf);
 				copybuf = NULL;
 				if (temp)
@@ -72,75 +68,43 @@ int rfile(int fd, char *buf, char **line)
 					free(temp);
 					temp = NULL;
 				}
-				break;
-            }
-        }
+				break ;
+			}
+		}
 		else if (bytes == 0)
 		{
-			*line = ft_strdup("cacota");
-			printf("zorra");
+			*line = ft_strdup("");
 			free(copybuf);
 			copybuf = NULL;
-        	return (0);
+			free(buf);
+			buf = NULL;
+			return (0);
 		}
 	}
-
-	//printf("Se ha liberado la memoria\n");
-	/*if (bytes == 0 && ft_strlen(copybuf) == 0)
-	{	
+	if (bytes == 0 && ft_strlen(copybuf) == 0 && i)
+	{
 		if (!(*line))
 			*line = ft_strdup("");
-	//		printf("Laputa");
-		free(copybuf);
-		copybuf = NULL;
+		if (!copybuf)
+		{
+			free(copybuf);
+			copybuf = NULL;
+		}
+		free(buf);
+		buf = NULL;
 		return (0);
-	}*/
+	}
 	return (1);
 }
 
-int get_next_line(int fd, char **line)
+int				get_next_line(int fd, char **line)
 {
-	//recived fd and create buffer(used for keep the text reading.)
-	char *buf;
-	//declarar varible estática
-	
+	char		*buf;
+
 	if (fd == -1 || !line || BUFFER_SIZE <= 0)
-	{
-		printf("Error al abrir archivo.\n");
 		return (-1);
-	}
-	buf = (char *)malloc(sizeof(char)*(BUFFER_SIZE+1));
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (buf == NULL)
 		return (-1);
-	return (rfile(fd, buf,line));
-		//Llamo a función asignar **line
+	return (rfile(fd, buf, line));
 }
-
-/*int main(int argc, char **argv)
-{
-//	char *str;
-
-//	if(get_next_line(1,&str) != -1)
-//		printf("%s", str);
-	int fd;
-	char *line = NULL;
-
-	argc++;
-	//abro archivo como solo lectura (O_RDONLY) y me devuelve el fd
-	fd = open(argv[1], O_RDONLY);
-//	get_next_line(fd, &line);
-//	printf("%s\n", line);
-//	free(line);
-//	line = NULL;
-	
-	//	system("leaks a.out");
-//	return (0);
-
-	 while (get_next_line(fd, &line) >  0)
-	{
-		write(1, line, ft_strlen(line) + 1);
-		write(1, "\n", 1);
-	}	
-//	//close(fd);
-	return (0);
-}*/
