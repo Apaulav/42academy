89d7f5859862de7bc90b4119de6032d16b0d07b3
Apaulav <50292334+Apaulav@users.noreply.github.com>
2020-02-28 13:03:07 +0100

    up

--- anvilleg/GNL/anvilleg/get_next_line.c
+++ anvilleg/GNL/anvilleg/get_next_line.c
@@ -6,105 +6,103 @@
 /*   By: anvilleg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 11:25:26 by anvilleg          #+#    #+#             */
-/*   Updated: 2020/02/25 20:06:13 by anvilleg         ###   ########.fr       */
+/*   Updated: 2020/02/27 23:05:19 by anvilleg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static	int		rfile(int fd, char *buf, char **line)
+static	void	clean(char *copybuf)
 {
-	ssize_t		bytes;
-	static char	*copybuf;
-	char		*temp;
-	int			i;
-	char		*aux;
+	free(copybuf);
+	copybuf = NULL;
+}
 
-	if (!copybuf)
-		copybuf = NULL;
-	temp = NULL;
-	while (buf)
+static int		jump(char *buf, char **copybuf, char **line, ssize_t bytes)
+{
+	char			*aux;
+	int				i;
+	char			*temp;
+
+	i = 0;
+	buf[bytes] = '\0';
+	aux = ft_strjoin(*copybuf, buf);
+	free(*copybuf);
+	*copybuf = aux;
+	while (*(*copybuf + i) != '\n' && *(*copybuf + i))
+		i++;
+	if (*(*copybuf + i) == '\n' || (*(*copybuf + i) == 0 && bytes == 0))
 	{
-		i = 0;
-		bytes = read(fd, buf, BUFFER_SIZE);
-		if (bytes < 0)
-		{
-			free(copybuf);
-			copybuf = NULL;
-			free(buf);
-			buf = NULL;
+		//temp = (*(*copybuf + i) == '\n') ? ft_strdup(&(*(*copybuf + i + 1)))
+		temp = (*(*copybuf + i) == '\n') ? ft_strdup((*copybuf + i + 1))
+			: ft_strdup("");
+		*line = ft_substr(*copybuf, 0, i);
+		if (!(*line))
 			return (-1);
-		}
-		else if (bytes > 0 || ft_strlen(copybuf) > 0)
-		{
-			buf[bytes] = '\0';
-			aux = ft_strjoin(copybuf, buf);
-			free(buf);
-			buf = NULL;
-			free(copybuf);
-			copybuf = NULL;
-			copybuf = ft_strdup(aux);
-			free(aux);
-			aux = NULL;
-			buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-			while (copybuf[i] != '\n' && copybuf[i])
-				i++;
-			if (copybuf[i] == '\n' || (copybuf[i] == 0 && bytes == 0))
-			{
-				if (temp)
-					free(temp);
-				if (copybuf[i] == '\n')
-					temp = ft_strdup(&copybuf[i + 1]);
-				else
-					temp = ft_strdup("");
-				*line = ft_substr(copybuf, 0, i);
-				if (!(*line))
-					return (-1);
-				free(copybuf);
-				copybuf = NULL;
-				if (temp)
-				{
-					copybuf = ft_strdup(temp);
-					free(temp);
-					temp = NULL;
-				}
-				break ;
-			}
-		}
-		else if (bytes == 0)
-		{
-			*line = ft_strdup("");
-			free(copybuf);
-			copybuf = NULL;
-			free(buf);
-			buf = NULL;
-			return (0);
-		}
+		free(*copybuf);
+		*copybuf = ft_strdup(temp);
+		free(temp);
+		return (1);
 	}
-	if (bytes == 0 && ft_strlen(copybuf) == 0 && i)
+	return (i);
+}
+
+static	int		validate(ssize_t bytes, char *copybuf, int j, char **line)
+{
+	if (bytes == 0 && ft_strlen(copybuf) == 0 && j)
 	{
 		if (!(*line))
 			*line = ft_strdup("");
-		if (!copybuf)
-		{
-			free(copybuf);
-			copybuf = NULL;
-		}
-		free(buf);
-		buf = NULL;
+		free(copybuf);
+		copybuf = NULL;
 		return (0);
 	}
 	return (1);
 }
 
+static	int		rfile(int fd, char *buf, char **line)
+{
+	ssize_t			bytes;
+	static char		*copybuf = NULL;
+	int				j;
+
+	if (!copybuf)
+		copybuf = ft_strdup("");
+	while (buf)
+	{
+		bytes = read(fd, buf, BUFFER_SIZE);
+		if (bytes < 0)
+			return (-1);
+		else if (bytes > 0 || ft_strlen(copybuf) > 0)
+		{
+			j = jump(buf, &copybuf, line, bytes);
+			if (j == 1)
+				return (1);
+		}
+		else if (bytes == 0)
+		{
+			*line = ft_strdup("");
+			clean(copybuf);
+			return (0);
+		}
+	}
+	return (validate(bytes, copybuf, j, line) ? 0 : 1);
+}
+
 int				get_next_line(int fd, char **line)
 {
 	char		*buf;
+	int			gnl;
 
-	if (fd == -1 || !line || BUFFER_SIZE <= 0)
+if (fd == -1 || !line || BUFFER_SIZE <= 0)
+	{
+		//printf("Error al abrir archivo.\n");
 		return (-1);
+	}
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (buf == NULL)
 		return (-1);
-	return (rfile(fd, buf, line));
+	gnl = rfile(fd, buf, line);
+	free(buf);
+	return (gnl);
 }
